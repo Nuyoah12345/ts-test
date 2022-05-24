c74a56df1c5f1a1965f6a036eea873362a182084
Nuyoah12345 <1715958569@qq.com>
2022-05-24 17:36:42 +0800

    新学习了遍历数组以及keyof关键字

---
+++ 11-easy-tuple-to-object/template.ts
@@ -0,0 +1,6 @@
+type TupleToObject<T extends readonly (string | number | symbol)[]> = {
+    [P in T[number]]: P
+}
+
+// const tuple = ['tesla', 'model 3', 'model X', 'model Y'] as const
+// type a = TupleToObject<typeof tuple>
